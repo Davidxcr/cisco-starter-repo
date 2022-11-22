b4a6e936f9683d72d3383f544df8123cb57554ad
Davidxcr <vodolatunji@gmail.com>
2022-11-22 17:05:18 -0600

    created exhibit and sextant components

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,21 +1,12 @@
 import './App.css';
+import Banner from './Banner';
+import Exhibit from './Exhibit';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Banner />
+      <Exhibit />
     </div>
   );
 }
